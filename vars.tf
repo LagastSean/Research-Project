b18ce3d1c0906e5985059b465e665b84495eac43
Lagast Sean <sean.lagast@student.howest.be>
2022-01-12 10:50:33 +0100

    eerste test code om omgeving in terraform op te zetten

---
+++ vars.tf
@@ -0,0 +1,33 @@
+variable "Amount" {
+    description = "Hoeveel webservers wil je deployen?"
+    type = number
+}
+
+variable "RG_Name" {
+    type = list
+    default = ["SeanLagast_Terraform","JoeMama_Terraform"]
+}
+
+variable "RG_Location" {
+    default = "westeurope"
+}
+
+variable "SP_Name" {
+    default = "WebAppServicePlan"
+}
+
+variable "SP_tier" {
+    default = "Free"
+}
+
+variable "SP_size" {
+    default = "F1"
+}
+
+variable "AS_bootstrap" {
+    default = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+}
+
+variable "AS_bootstrap_branch" {
+    default = "master"
+}
